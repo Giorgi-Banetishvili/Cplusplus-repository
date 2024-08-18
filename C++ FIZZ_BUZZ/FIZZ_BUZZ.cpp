4db906848a3bf4fb2a40265e6335809b701a9489
Giorgi-Banetishvili <georgebanetishvili@gmail.com>
2024-08-18 10:50:06 +0400

    Add files via upload

---
+++ C++ FIZZ_BUZZ/FIZZ_BUZZ.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+
+    for (int i = 1; i <= 15; i++)
+    {
+
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+
+            std::cout << "FIZZBUZZ" << "\n";
+        }
+        else if (i % 3 == 0)
+        {
+
+            std::cout << "FIZZ" << "\n";
+        }
+        else if (i % 5 == 0)
+        {
+
+            std::cout << "BUZZ" << "\n";
+        }
+        else
+        {
+            std::cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
